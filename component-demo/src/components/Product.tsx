588c8db887ad816849a3ac7808e051de56b6cc34
Julien Adam <julien.adam@gmail.com>
2022-12-09 13:01:30 +0100

    Better component demo with CSS

--- component-demo/src/components/Product.tsx
+++ component-demo/src/components/Product.tsx
@@ -5,10 +5,11 @@ class Product extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className='Product'>
+        <div><b>Category:</b> <em>Core modules</em></div>
         <div><b>Name:</b> <em>Frameshift Drive</em></div>
         <div><b>Class:</b> <em>5D</em></div>
-        <div><b>Cost:</b> <em>25000</em></div>
+        <div><b>Cost:</b> <em className='Product-cost'>25000</em>cr</div>
       </div>
       );
   }
