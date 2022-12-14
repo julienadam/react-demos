a31147b87fddcf7af64344f445d732e8af0fb6e9
Julien Adam <julien.adam@arcodia.fr>
2022-12-14 20:13:13 +0100

    Created form demo

---
+++ form-demo/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NameForm from './components/NameFormDemo/NameFormDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NameForm></NameForm>
+      </header>
+    </div>
+  );
+}
+
+export default App;
