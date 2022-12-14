d09ea75e3328f2e4710f3f191ad9c7722ef73e61
Julien Adam <julien.adam@arcodia.fr>
2022-12-14 21:26:36 +0100

    Formik demo

---
+++ formik-demo/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ValidatedFormDemo from './components/ValidatedFormDemo';
+
+function App() {
+  return (
+    <ValidatedFormDemo></ValidatedFormDemo>
+  );
+}
+
+export default App;
