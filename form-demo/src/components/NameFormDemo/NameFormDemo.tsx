ac0225fcabc2416cd59cb5f68f98d84ef0fefcc7
Julien Adam <julien.adam@arcodia.fr>
2022-12-14 20:20:43 +0100

    Better demo showing state being changed

--- form-demo/src/components/NameFormDemo/NameFormDemo.tsx
+++ form-demo/src/components/NameFormDemo/NameFormDemo.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 
-class NameForm extends React.Component<{}, {value: string}> {
-  constructor(props : {}) {
+class NameForm extends React.Component<{}, { value: string }> {
+  constructor(props: {}) {
     super(props);
     this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event : React.ChangeEvent<HTMLInputElement>) { this.setState({ value: event.target.value }); }
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}><label> Name:
-        <input type="text" value={this.state.value} onChange={this.handleChange} /></label>
-        <input type="submit" value="Submit" />
-      </form>);
+      <div>
+        <p>Current data : {this.state.value}</p>
+        <form onSubmit={this.handleSubmit}><label> Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} /></label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      );
   }
 }
-
 export default NameForm;
