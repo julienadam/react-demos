08e5b3871c3c4177c5e6f51746e8a9fd49fe3aec
Julien Adam <julien.adam@arcodia.fr>
2022-12-15 13:22:53 +0100

    Used header component

--- router-demo/src/App.tsx
+++ router-demo/src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 
-
-export const Home = () => <h1>Home Page</h1>;
-export const Foo = () => <h1>Foo Page</h1>;
-export const Bar = () => <h1>Bar Page</h1>;
+const Home = () => <h1>Home Page</h1>;
+const Foo = () => <h1>Foo Page</h1>;
+const Bar = () => <h1>Bar Page</h1>;
 
 class App extends React.Component {
   render() {
     return (
-      <div><h1>Header</h1>
+      <div>
       <Router>
+        <Header></Header>
         <div>
-          <nav>
-            <Link to="/">Home</Link> | &nbsp;
-              <Link to="/foo">Foo</Link> | &nbsp;
-            <Link to="/bar">Bar</Link>
-          </nav>
           <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/foo" element={<Foo />} />
--- router-demo/src/components/Header/Header.tsx
+++ router-demo/src/components/Header/Header.tsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 function Header() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/survey">Survey</Link>
+      <Link to="/">Home</Link> | &nbsp;
+      <Link to="/Foo">Foo</Link> | &nbsp;
+      <Link to="/Bar">Bar</Link>
     </nav>
   )
 }
